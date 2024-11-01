bcab531f6d3190c13cf5dba362a7c79e25e723b0
Titus672 <titusfreezn@gmail.com>
2024-11-01 10:12:04 -0700

    test fix

--- psql_functions.py
+++ psql_functions.py
@@ -62,7 +62,7 @@ class DB:
             signal_remote_60g
             )
                 VALUES (%s, %s, %s, %s, %s)
-                ON CONFLICT (time) DO NOTHING
+                ON CONFLICT (time, device_id) DO NOTHING
         """
         self.cursor.execute(data_q, data)
     
